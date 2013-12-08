cce5d5bf890f5db2c9de15554887dd34507ea429
Max Wayt <max@wayt.me>
2013-12-08 21:43:05 +0100

    Update Cmake build system

--- src/shared/ServerSocket/TcpSocket.cpp
+++ src/shared/ServerSocket/TcpSocket.cpp
@@ -1,4 +1,6 @@
 #include "TcpSocket.hpp"
+#include <iostream>
+#include <boost/bind.hpp>
 
 TcpSocket::TcpSocket(boost::asio::io_service& service) :
     _socket(service)
@@ -7,17 +9,23 @@ TcpSocket::TcpSocket(boost::asio::io_service& service) :
     _socket.set_option(option);
 }
 
-void SessionSocket::close()
+void TcpSocket::close()
 {
     boost::system::error_code ec;
     _socket.shutdown(boost::asio::ip::tcp::socket::shutdown_both, ec);
     _socket.close();
 }
 
-void SessionSocket::send(uint8 const* data, uint16 size)
+void TcpSocket::send(uint8 const* data, uint16 size)
 {
     boost::asio::async_write(_socket,
             boost::asio::buffer(data, size),
-            boost::bind(&SessionSocket::_handleWrite, this,
+            boost::bind(&TcpSocket::_handleWrite, this,
                 boost::asio::placeholders::error));
+}
+
+void TcpSocket::_handleWrite(boost::system::error_code const& error)
+{
+    if (error)
+        std::cerr << "Error: " << error.message() << std::endl;
 }
